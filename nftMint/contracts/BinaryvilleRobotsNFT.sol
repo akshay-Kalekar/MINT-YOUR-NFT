b8f924832f2bac02ac5a45d266f0245ef5b66ef7
Akshay A Kalekar <50509605+akshay-Kalekar@users.noreply.github.com>
2022-11-04 19:50:00 +0530

    Add files via upload

---
+++ nftMint/contracts/BinaryvilleRobotsNFT.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract BinaryvilleRobotsNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("BinaryvilleRobotsNFT", "BR") {}
+
+    function mintNFT(address recipient, string memory tokenURI)public returns (uint256)
+    {
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _tokenIds.increment();
+        return newItemId;
+    }
+}
